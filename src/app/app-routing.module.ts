bd8865dac81bc84449a9bda3f1b9457ff5d4a49e
romulosant <150974641+romulosant@users.noreply.github.com>
2024-09-20 00:30:59 -0300

    rota

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,29 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 import { CadastroPage } from './cadastro/cadastro.page';
+import { Tab1Page } from './tab1/tab1.page';
+import { TabsPage } from './tabs/tabs.page';
 
 const routes: Routes = [
+  { path: 'tab1', loadChildren: () => import('./tab1/tab1.module').then(m => m.Tab1PageModule) },
+  { path: 'tab2', loadChildren: () => import('./tab2/tab2.module').then(m => m.Tab2PageModule) },
+  { path: 'tab3', loadChildren: () => import('./tab3/tab3.module').then(m => m.Tab3PageModule) },
+  { path: '', redirectTo: 'tab2', pathMatch: 'full' } ,
   {
     path: 'cadastro',
     component: CadastroPage
   },
   {
+    path: 'tabs',
+    component: TabsPage,
+  },
+  {
+    path: 'tabs',
+    loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+  },
+  {
     path: '',
-    redirectTo: 'inicial', // Redireciona para a página inicial
+    redirectTo: 'inicial', 
     pathMatch: 'full'
   },
   {
@@ -43,7 +57,7 @@ const routes: Routes = [
   {
     path:'tab1',
     loadChildren: () => import('./tab3/tab3.module').then(m => m.Tab3PageModule)
-  }
+  },
 ];
 
 @NgModule({
--- src/app/tabs/tabs-routing.module.ts
+++ src/app/tabs/tabs-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { TabsPage } from './tabs.page';
 
 const routes: Routes = [
+  
   {
     path: 'tabs',
     component: TabsPage,
